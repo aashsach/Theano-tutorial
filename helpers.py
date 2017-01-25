8bfa3d7b3079207d17df1546465028dcbb59bde0
aashsach <aashraya.sachdeva@csa.iisc.ernet.in>
2017-01-25 21:58:50 +0530

    first commit

---
+++ helpers.py
@@ -0,0 +1,32 @@
+import numpy as np
+import gzip
+def load_dataset(path='./data/'):
+    def load_mnist_images(filename):
+        with gzip.open(filename, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=16)
+        data = data.reshape(-1, 1, 28, 28)
+        return data / np.float32(256)
+
+    def load_mnist_labels(filename):
+        with gzip.open(filename, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=8)
+        return data
+    X_train = load_mnist_images(path+'train-images-idx3-ubyte.gz')
+    y_train = load_mnist_labels(path+'train-labels-idx1-ubyte.gz')
+    X_test = load_mnist_images(path+'t10k-images-idx3-ubyte.gz')
+    y_test = load_mnist_labels(path+'t10k-labels-idx1-ubyte.gz')
+    X_train, X_val = X_train[:-10000], X_train[-10000:]
+    y_train, y_val = y_train[:-10000], y_train[-10000:]
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
+    assert len(inputs) == len(targets)
+    if shuffle:
+        indices = np.arange(len(inputs))
+        np.random.shuffle(indices)
+    for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
+        if shuffle:
+            excerpt = indices[start_idx:start_idx + batchsize]
+        else:
+            excerpt = slice(start_idx, start_idx + batchsize)
+        yield inputs[excerpt], targets[excerpt]
